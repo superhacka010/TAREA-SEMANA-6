0d6c2d97c403d0d40bef6c4733b7300e14f39a9b
superhacka010 <super.hacka0101@gmail.com>
2026-05-13 10:22:55 -0500

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,11 @@
+public class Main1 {
+ 
+    // TODO: Implementa tu clase Stack aqui
+ 
+    // TODO: Implementa el metodo para verificar si una cadena es palindromo usando la pila
+ 
+    public static void main(String[] args) {
+        // TODO: Verifica si "racecar" y "hello" son palindromos e imprime el resultado
+    }
+}
+ 
