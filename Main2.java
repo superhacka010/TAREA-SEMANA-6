f4183d1a9183d7d97d26a12cb051540dd89d779b
superhacka010 <super.hacka0101@gmail.com>
2026-05-13 10:23:43 -0500

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+ 
+    // TODO: Implementa tu clase Stack aqui
+ 
+    // TODO: Implementa el algoritmo de Torres de Hanoi usando la pila
+ 
+    public static void main(String[] args) {
+        // TODO: Resuelve Torres de Hanoi con 3 discos de la torre A a la torre C usando B como auxiliar
+    }
+}
